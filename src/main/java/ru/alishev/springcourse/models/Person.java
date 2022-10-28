43afbbc45ec09fe22b53b0bd58526239c36814d2
FreakPunchess <aleksandr-pisklov@mail.ru>
2022-10-28 18:58:56 +0300

    My first commit

---
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -0,0 +1,53 @@
+package ru.alishev.springcourse.models;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Neil Alishev
+ */
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Поле имя не может быть пустым")
+    @Size(min = 2, max = 30, message = "Имя должно содержать от 2 до 100 символов")
+    private String fullName;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше чем 1900")
+    private int yearOfBirth;
+
+    public Person() {
+
+    }
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+}
